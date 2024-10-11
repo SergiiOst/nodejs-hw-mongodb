68cbecd2fd765eeca74f55fda8509501373cb99e
Sergii <sergeyostapenko85@gmail.com>
2024-10-11 15:59:54 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -59,7 +59,7 @@ export const createContactController = async (reg, res) => {
 
 export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const contact = await deleteContact(contactId);
+  const contact = await deleteContact(contactId, req.user._id);
 
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
@@ -69,9 +69,9 @@ export const deleteContactController = async (req, res, next) => {
   res.status(204).send();
 };
 
-export const patchContactController = async (reg, res, next) => {
-  const { contactId } = reg.params;
-  const result = await updateContact(contactId, reg.body);
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.user._id, req.body);
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
